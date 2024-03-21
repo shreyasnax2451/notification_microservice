cb1de066917ee08952ea67214e8dd4909f3f60d1
shreyasnax2451 <shreyasnax2451@gmail.com>
2024-03-21 21:07:29 +0530

    replaced SMTP with AWS SES

--- src/services/notifications/interaction/send_notification.py
+++ src/services/notifications/interaction/send_notification.py
@@ -1,56 +1,75 @@
 from configs.env import *
-import smtplib
+import re
+import boto3
 from fastapi import HTTPException
 from database.db_session import db
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from services.notification_templates.interaction.get_notification_template import get_notification_template
 from services.notifications.models.notification import Notification
 
-def send_email_notification(request):
-    notification_data = get_notification_template({
-        'type':request['template_data']['type'],
-        'name':request['template_data']['name']
-    })
-    html_content = notification_data['html_template']
-    for key in request['notification_data']:
-        html_content = html_content.replace(f'{key}', request['notification_data'].get(key))
+def send_email_notification(request, notification_data):
     try:
-        sender_email = EMAIL_ADDRESS_SMPT
-        recipient_email = EMAIL_ADDRESS_SMPT
-        password = PASSWORD_SMPT
+        message_content = notification_data['message_template']
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = notification_data['subject']
-        message["From"] = sender_email
-        message["To"] = recipient_email
+        def replace_placeholders(match):
+            key = match.group(1)
+            return request['notification_data'].get(key, f'<{key} not found>')
+        message_content = re.sub('{{(.*?)}}', replace_placeholders, message_content)
+        subject = re.sub('{{(.*?)}}', replace_placeholders, notification_data['subject'])
 
-        html_part = MIMEText(html_content, "html")
-        message.attach(html_part)
-
-        with smtplib.SMTP("smtp.gmail.com", "587") as connection:
-            # server.login(sender_email, password)
-            # server.sendmail(sender_email, recipient_email, message.as_string())
-            connection.starttls()
-            connection.login(user = sender_email, password = password)
-            connection.sendmail(
-                from_addr = sender_email, 
-                to_addrs = recipient_email, 
-                msg = message.as_string()
-            )
+        RECIPIENT = request['email_address']
+        SUBJECT = subject
+        BODY_TEXT = ("Notification AWS Test\r\n"
+                    "This notification was sent with Amazon SES using the "
+                    "AWS SDK for Python (Boto)."
+                    ) 
+        BODY_HTML = message_content
+        session = boto3.Session(
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        )
+        client = session.client('ses',region_name=AWS_REGION)
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+        )
         return True
     except Exception as e:
-        print(e)
-        print('Email Failed!')
+        raise HTTPException(status_code=500, detail=e)
     
 def send_notification(request):
-    with db.atomic():
-        if send_email_notification(request):
-            notification_data = request['notification_data'] | request['template_data']
-            notification = Notification.create(
-                notification_data = notification_data,
-                user_id = request['user_id']
-            )
-            return notification.id
-        else:
-            raise HTTPException(status_code=500, detail='Notification Not Sent')
+    notification_template_data = get_notification_template({
+        'template_type':request['template_data']['template_type'],
+        'template_name':request['template_data']['template_name']
+    })
+    if notification_template_data:
+        with db.atomic():
+            if send_email_notification(request, notification_template_data):
+                notification_data = request['notification_data'] | request['template_data']
+                notification = Notification.create(
+                    notification_data = notification_data,
+                    user_id = request['user_id'],
+                    notification_template_id = notification_template_data['id']
+                )
+                return {"id":notification.id}
+    else:
+        raise HTTPException(status_code=404, detail='Template Not Found')
