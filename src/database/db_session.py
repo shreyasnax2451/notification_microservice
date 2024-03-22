3cb7f07bddc7c7b22bd0604db44d0c5e20a8acf7
shreyasnax2451 <shreyasnax2451@gmail.com>
2024-03-22 14:53:08 +0530

    db session changes

--- src/database/db_session.py
+++ src/database/db_session.py
@@ -1,11 +1,17 @@
 from peewee import PostgresqlDatabase
 from configs.env import *
-import logging
-from contextvars import ContextVar
 
-db = PostgresqlDatabase(
-    DATABASE_NAME,
-    autorollback=True,
-    user = DATABASE_USER,
-    password = DATABASE_PASSWORD
-)
+db_params = {
+    "database": DATABASE_NAME,
+    "user": DATABASE_USER,
+    "password": DATABASE_PASSWORD,
+    "autorollback": True
+}
+
+class CustomDatabase(PostgresqlDatabase):
+    def execute_sql(self, sql, params=None, commit=object()):
+        if db.is_closed():
+            db.connect(reuse_if_open = True)
+        return super().execute_sql(sql, params, commit)
+
+db = CustomDatabase(**db_params)
